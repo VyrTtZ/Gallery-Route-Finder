ea29daefc47a6d2d07adfac92d5a1c7771b44570
Bao <20108791@mail.wit.ie>
2026-03-24 12:44:46 +0000

    Added visualizing abilities, polished the Dijkstra.

--- src/main/java/galleryRouteFinder/main/MainController.java
+++ src/main/java/galleryRouteFinder/main/MainController.java
@@ -3,13 +3,22 @@ package galleryRouteFinder.main;
 import galleryRouteFinder.structs.Edge;
 import galleryRouteFinder.structs.Vertex;
 import galleryRouteFinder.utilities.Utils;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import javafx.util.Pair;
 
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.*;
 import java.io.File;
 import java.io.IOException;
@@ -18,94 +27,100 @@ public class MainController {
     //Front end
     public TextField startingRoom, endingRoom;
     public ImageView imageView;
+    public AnchorPane imagePane;
+    public Button shortestPathToggle;
 
     //Back end
     private static final double SCALE=0.7; //Scale of the map in the program
-    private boolean shortestPathAlgorithm=false; //False=Dijkstra, true=bfs,
+    private boolean shortestPathAlgorithm=false; //True=Dijkstra, false=bfs,
 
     ArrayList <Vertex> vertices=new ArrayList <>();
-    ArrayList <Edge> edges;
+    ArrayList <Edge> edges=new ArrayList <>();
 
     public void initialize()
     {
         getVertices();
         getEdges();
+        shortestPathToggle.setOnAction(e->{
+            shortestPathAlgorithm=!shortestPathAlgorithm;
+            shortestPathToggle.setText(shortestPathAlgorithm? "Dijkstra":"BFS");
+        });
         imageView.setImage(new Image("map.jpg"));
     }
 
     public void getVertices()
     {
-        File file=new File("vertices.csv");
-        try (Scanner read=new Scanner(file))
+        InputStream is = getClass().getResourceAsStream("/vertices.csv");
+        int skips=3;
+        Scanner read=new Scanner(is);
+        while (read.hasNextLine())
         {
-            while (read.hasNextLine())
+            String line=read.nextLine();
+            if (skips>0)
             {
-                String line=read.nextLine();
-                //Format: ID, x, y, name
-                int start=0;
-                Vertex vertex=new Vertex();
-                for (int i=0; i<4; i++)
-                {
-                    String tmp=Utils.commaStringExtraction(line, start);
-                    start+=tmp.length()+1;
-                    int data=0;
-                    if (i!=3)
-                        data=Integer.parseInt(tmp);
-                    switch (i)
-                    {
-                        case 0 -> vertex.setId(data);
-                        case 1 -> vertex.setPosX(data);
-                        case 2 -> vertex.setPosY(data);
-                        case 3 -> vertex.setName(tmp);
-                    }
-                }
-                vertices.add(vertex);
+                skips--;
+                continue;
             }
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
+            //Format: ID, x, y, name
+            int start=0;
+            Vertex vertex=new Vertex();
+            for (int i=0; i<4; i++)
+            {
+                String tmp=Utils.commaStringExtraction(line, start);
+                start+=tmp.length()+1;
+                int data=0;
+                if (i!=3)
+                    data=Integer.parseInt(tmp);
+                switch (i)
+                {
+                    case 0 -> vertex.setId(data);
+                    case 1 -> vertex.setPosX(data);
+                    case 2 -> vertex.setPosY(data);
+                    case 3 -> vertex.setName(tmp);
+                }
+            }
+            vertices.add(vertex);
         }
     }
 
     public void getEdges()
     {
-        File file=new File("edges.csv");
-        try (Scanner read=new Scanner(file))
+        InputStream is = getClass().getResourceAsStream("/edges.csv");
+        int skips=3;
+        Scanner read=new Scanner(is);
+        while (read.hasNextLine())
         {
-            while (read.hasNextLine())
+            String line=read.nextLine();
+            if (skips>0)
             {
-                String line=read.nextLine();
-                //Format: id1, id2, x, y, x, y,...
-                int start=0, i=0;
-                Edge edge=new Edge(getVertex(0), getVertex(0));
-                while (start<line.length())
-                {
-                    String tmp=Utils.commaStringExtraction(line, start);
-                    start+=tmp.length()+1;
-                    int data=Integer.parseInt(tmp);
-                    if (i==0)
-                        edge.setNode1(getVertex(data));
-                    else if (i==1)
-                        edge.setNode2(getVertex(data));
-                    else
-                    {
-                        int[] tmpArray=new int[2];
-                        if (i%2==0) {
-                            tmpArray[0] = data;
-                            edge.getIntermediaryNodes().add(tmpArray);
-                        }
-                        else
-                            edge.getIntermediaryNodes().getLast()[1]=data;
-                    }
-                    i++;
-                }
-                edges.add(edge);
+                skips--;
+                continue;
             }
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
+            //Format: id1, id2, x, y, x, y,...
+            int start=0, i=0;
+            Edge edge=new Edge(getVertex(20), getVertex(20));
+            while (start<line.length())
+            {
+                String tmp=Utils.commaStringExtraction(line, start);
+                start+=tmp.length()+1;
+                int data=Integer.parseInt(tmp);
+                if (i==0)
+                    edge.setNode1(getVertex(data));
+                else if (i==1)
+                    edge.setNode2(getVertex(data));
+                else
+                {
+                    int[] tmpArray=new int[2];
+                    if (i%2==0) {
+                        tmpArray[0] = data;
+                        edge.getIntermediaryNodes().add(tmpArray);
+                    }
+                    else
+                        edge.getIntermediaryNodes().getLast()[1]=data;
+                }
+                i++;
+            }
+            edges.add(edge);
         }
         for (Vertex vertex : vertices)
         {
@@ -122,27 +137,26 @@ public class MainController {
 
     public void shortestPath()
     {
+        ArrayList <Integer> res=new ArrayList<>();
         if (shortestPathAlgorithm) //Diji
         {
-            ArrayList <Integer> res=findShortestPathDijkstra();
-            for (int i=0; i<res.size(); i++)
-                System.out.println(res.get(i));
+            res=findShortestPathDijkstra();
         }
-        else
-        {
-            int startId=Integer.parseInt(startingRoom.getText()), endId=Integer.parseInt(endingRoom.getText());
-            Vertex startV = getVertex(startId), endV=getVertex(endId);
+        else {
+            int startId = Integer.parseInt(startingRoom.getText()), endId = Integer.parseInt(endingRoom.getText());
+            Vertex startV = getVertex(startId), endV = getVertex(endId);
             LinkedList<Vertex> path = Vertex.BFS(startV, endV, null);
-            for(Vertex v : path) System.out.println(v.getId());
+            for (Vertex v : path) res.add(v.getId());
         }
+        visualizePath(res);
     }
 
     private ArrayList<Integer> findShortestPathDijkstra()
     {
         //TODO Add validation
         int startId=Integer.parseInt(startingRoom.getText()), endId=Integer.parseInt(endingRoom.getText());
-        boolean[] visited=new boolean[vertices.size()];
-        Vertex[] prev=new Vertex[vertices.size()];
+        boolean[] visited=new boolean[25];
+        Vertex[] prev=new Vertex[25]; //vertices.size()
         Vertex startVertex=getVertex(startId), endVertex=getVertex(endId);
         PriorityQueue <Pair<Pair<Vertex, Vertex>, Double>> queue=new PriorityQueue<>((o1, o2) -> {
             if (o1.getValue()<o2.getValue())
@@ -174,7 +188,35 @@ public class MainController {
             path.add(endVertex.getId());
             endVertex=prev[endVertex.getId()];
         }
-        return path;
+        path.add(startVertex.getId());
+        ArrayList<Integer> res=new ArrayList<>();
+        for (int i=path.size()-1; i>=0; i--)
+            res.add(path.get(i));
+        return res;
+    }
+
+    private void visualizePath(List<Integer> path)
+    {
+        SequentialTransition sequence=new SequentialTransition();
+        imagePane.getChildren().removeIf(node -> node instanceof Circle || node instanceof Label);
+        for (Integer integer : path) {
+            PauseTransition circleDraw = new PauseTransition(Duration.millis(200));
+            Circle circle = new Circle();
+            Vertex vertex = getVertex(integer);
+            circle.setCenterX(vertex.getPosX() * SCALE);
+            circle.setCenterY(vertex.getPosY() * SCALE);
+            circle.setStroke(Color.YELLOW);
+            circle.setStrokeWidth(2);
+            circle.setFill(Color.TRANSPARENT);
+            circle.setRadius(7);
+
+            Label label = new Label(vertex.getId() + "");
+            label.setLayoutX(vertex.getPosX() * SCALE-7);
+            label.setLayoutY(vertex.getPosY() * SCALE-7);
+            circleDraw.setOnFinished(event -> {imagePane.getChildren().addAll(circle, label);});
+            sequence.getChildren().add(circleDraw);
+        }
+        sequence.playFromStart();
     }
 
     private Vertex getVertex(int id)
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module galleryRouteFinder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
     opens galleryRouteFinder.main to javafx.fxml;
     exports galleryRouteFinder.main;
     exports galleryRouteFinder.structs;
